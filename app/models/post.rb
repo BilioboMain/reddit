b728c7a49a26907a3e7d97218ff95d0b430a2df1
gkrotov <Gleb.Krotov@noveogroup.com>
2023-09-09 16:30:46 +0300

    added relations

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,5 @@ class Post < ApplicationRecord
   validates :title, length: { in: 2..40 }
   validates :body, length: { in: 2..400}
 
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
   validates :username, uniqueness: true, length: {minimum: 2}
   validates :password, length: { in: 6..20 }
+
+  has_many :posts
 end
