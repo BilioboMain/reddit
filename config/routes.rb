4df46e07dc7383a3570a123bc2b143e771f6c057
gkrotov <Gleb.Krotov@noveogroup.com>
2023-09-24 18:23:45 +0300

    added registarions

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,15 @@
 Rails.application.routes.draw do
+  get 'registrations/new'
+
   root 'home#index'
   get 'home', to: 'home#index'
+
   resources :posts
+
+  resources :users
+
+  get 'sign_up', to: 'registrations#new'
+  post 'sign_up', to: 'registrations#create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_09_165310) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_24_145703) do
   create_table "comments", force: :cascade do |t|
     t.integer "user_id", null: false
     t.text "body"
@@ -35,6 +35,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_09_165310) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
   add_foreign_key "comments", "posts"
